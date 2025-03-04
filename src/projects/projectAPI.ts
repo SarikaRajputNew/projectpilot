842fa8394d5c12233d6d0f64d63176db353d891f
sarasingh <ssingh@smartfutures.org>
2025-03-04 13:00:43 +0530

    lab23

--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -52,7 +52,7 @@ function convertToProjectModel(item: any): Project {
 const projectAPI = {
   get(page = 1, limit = 20) {
     return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
-      .then(delay(600))
+      .then(delay(2000))
       .then(checkStatus)
       .then(parseJSON)
       .then(convertToProjectModels)
@@ -71,6 +71,7 @@ const projectAPI = {
         "Content-Type": "application/json",
       },
     })
+      .then(delay(2000))
       .then(checkStatus)
       .then(parseJSON)
       .catch((error: TypeError) => {
